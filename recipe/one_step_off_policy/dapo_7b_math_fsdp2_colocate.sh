c2395f7b13d8bf61014bf65c0c6f8663f843eb5d
ArronHZG <hou.zg@foxmail.com>
2025-07-11 16:11:51 +0800

    config

--- recipe/one_step_off_policy/dapo_7b_math_fsdp2_colocate.sh
+++ recipe/one_step_off_policy/dapo_7b_math_fsdp2_colocate.sh
@@ -47,12 +47,12 @@ top_k=-1 # 0 for HF rollout, -1 for vLLM rollout
 val_top_p=0.7
 
 # Performance Related Parameter
-sp_size=4
 use_dynamic_bsz=True
 actor_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 2))
 infer_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 3))
 offload=True
-gen_tp=4
+gen_tp=2
+sp_size=4
 fsdp_size=2
 
 # reference run wandb: https://wandb.ai/verl-org/DAPO%20Reproduction%20on%20verl/runs/ow47vvon?nw=nwusertongyuxuan361

165c1b2a80e03afdd292bb1d52c6d3f370b72fd7
lalala-2 <yrzr12345678@gmail.com>
2025-07-11 17:12:37 +0800

    Revert "fix config"
    This reverts commit 5ea1c00749057e11ce6c4627cd465214b0763a96.

--- recipe/one_step_off_policy/async_main_ppo.py
+++ recipe/one_step_off_policy/async_main_ppo.py
@@ -16,47 +16,21 @@
 Note that we don't combine the main with ray_trainer as ray_trainer is used by other main.
 """
 
-import copy
 import os
 import socket
 
 import hydra
 import ray
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import OmegaConf
 
 from verl.trainer.main_ppo import create_rl_dataset, create_rl_sampler
 from verl.trainer.ppo.reward import load_reward_manager
-from verl.utils import omega_conf_to_dataclass
 
 from .async_ray_trainer import AsyncRayPPOTrainer
 
-
-def trainer_dict_to_dataclass(conf: DictConfig):
-    """Convert specific nested sections of a DictConfig object into dataclass instances.
-
-    Args:
-        conf (DictConfig): An instance of DictConfig, typically from the omegaconf library,
-                           representing a configuration dictionary.
-
-    Returns:
-        DictConfig: A deep copy of the input `conf` with specific sections converted to dataclasses.
-    """
-    # Create a deep copy of the input configuration to avoid modifying the original object
-    config = copy.deepcopy(conf)
-    config.algorithm = omega_conf_to_dataclass(config.algorithm)
-    config.critic.profiler = omega_conf_to_dataclass(config.critic.profiler)
-    config.reward_model.profiler = omega_conf_to_dataclass(config.reward_model.profiler)
-    config.actor_rollout_ref.actor.profiler = omega_conf_to_dataclass(config.actor_rollout_ref.actor.profiler)
-    config.actor_rollout_ref.ref.profiler = omega_conf_to_dataclass(config.actor_rollout_ref.ref.profiler)
-    config.actor_rollout_ref.rollout.profiler = omega_conf_to_dataclass(config.actor_rollout_ref.rollout.profiler)
-    return config
-
-
 @hydra.main(config_path="config", config_name="async_ppo_trainer", version_base=None)
-def main(config_dict):
-    config = trainer_dict_to_dataclass(config_dict)
+def main(config):
     run_ppo(config)
-
 
 # Define a function to run the PPO-like training process
 def run_ppo(config) -> None:
